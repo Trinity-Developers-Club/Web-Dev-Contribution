cb2aca1b6f95deeb4f2bba97723e3506d398f304
Dhruv varshney <83370198+Dhruv-IGI@users.noreply.github.com>
2024-10-06 00:56:16 +0530

    Add files via upload

---
+++ React/dhruv/app.js
@@ -0,0 +1,35 @@
+let inputBox = document.getElementById('inputBox')
+let buttons = document.querySelectorAll('button')
+
+let string = ''
+
+buttons.forEach(element =>{
+    element.addEventListener('click', (b)=>{
+        if(b.target.innerText == '='){
+         string = string == '' ? '0': string;
+         try{
+            string = String(eval(string))
+         inputBox.value = string;
+         }
+         catch(error){
+            inputBox.value = "Error(Press AC)"
+         }
+        }
+        else if(b.target.innerText == 'AC'){
+            string = ''
+            inputBox.value = string;
+        }
+        else if(b.target.innerText == 'DEL'){
+            string = string.substring(0,string.length-1)
+            inputBox.value = string;
+        }
+        else if(b.target.id == 'plusMinus'){
+            string = String(-eval(string))
+            inputBox.value = string;
+        }
+        else{
+            string += b.target.innerText
+            inputBox.value = string
+        }
+    })
+})
